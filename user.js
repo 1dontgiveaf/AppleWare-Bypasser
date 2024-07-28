83886442db15a0e11c0ca412faa6e9913dfae76a
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2024-07-28 15:02:51 -0400

    Create user.js

---
+++ user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        AppleWare Bypass
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @match        *://appleware.dev/start/*
+// @grant       none
+// @version     2.7
+// @author      .idontgiveaf
+// @description https://discord.gg/5vNgzbUsA3 join for support (untested this script so please test for me)
+// @license     MIT
+// @run-at      document-start
+// @namespace   https://github.com/1dontgiveaf/AppleWare-Bypasser/
+// @downloadURL https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/user.js
+// @updateURL   https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/meta.js
+// ==/UserScript==
