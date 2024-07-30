d12950d4f45c7137dc23958bc04c0dbf85a2cb2e
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2024-07-30 11:00:33 -0400

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,5 +1,10 @@
 // ==UserScript==
-// @name        AppleWare Bypass
+// @name        Apple-Ware Bypasser
+// @author      .idontgiveaf
+// @version     3.0
+// @description https://discord.gg/5vNgzbUsA3 this took me 3 days to learn & make, please praise (No sleep)
+// @license     MIT
+// @run-at      document-start
 // @match        *://loot-link.com/s?*
 // @match        *://loot-links.com/s?*
 // @match        *://lootlink.org/s?*
@@ -9,13 +14,7 @@
 // @match        *://lootdest.com/s?*
 // @match        *://links-loot.com/s?*
 // @match        *://linksloot.net/s?*
-// @match        *://appleware.dev/start/*
 // @grant       none
-// @version     2.7
-// @author      .idontgiveaf
-// @description https://discord.gg/5vNgzbUsA3 join for support (untested this script so please test for me)
-// @license     MIT
-// @run-at      document-start
 // @namespace   https://github.com/1dontgiveaf/AppleWare-Bypasser/
 // @downloadURL https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/user.js
 // @updateURL   https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/meta.js
